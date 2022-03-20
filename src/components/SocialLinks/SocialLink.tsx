c7d512e58e1a1d673d2cd81d61320eb500715721
Darshan Ponikar <darshanp@geekyants.com>
2022-03-20 16:59:21 +0530

    Links added

--- src/components/SocialLinks/SocialLink.tsx
+++ src/components/SocialLinks/SocialLink.tsx
@@ -4,11 +4,31 @@ import { SocialIcon } from "../SocialIcon";
 export const SocialLink: FC = () => {
   return (
     <div className="flex items-center gap-4">
-      <SocialIcon src="gh.svg" alt="Github Logo" />
-      <SocialIcon src="tw.svg" alt="Twitter Logo" />
-      <SocialIcon src="ld.svg" alt="Linkedin Logo" />
-      <SocialIcon src="dev.svg" alt="Dev Community Logo" />
-      <SocialIcon src="discord.svg" alt="Discord Logo" />
+      <SocialIcon
+        href="https://github.com/ponikar"
+        src="gh.svg"
+        alt="Github Logo"
+      />
+      <SocialIcon
+        href="https://twitter.com/iponikar"
+        src="tw.svg"
+        alt="Twitter Logo"
+      />
+      <SocialIcon
+        href="https://www.linkedin.com/in/ponikar/"
+        src="ld.svg"
+        alt="Linkedin Logo"
+      />
+      <SocialIcon
+        href="https://dev.to/ponikar"
+        src="dev.svg"
+        alt="Dev Community Logo"
+      />
+      <SocialIcon
+        href="https://discord.com/users/iponikar#1976"
+        src="discord.svg"
+        alt="Discord Logo"
+      />
     </div>
   );
 };
