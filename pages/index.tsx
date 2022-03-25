6b9607eff1c4e0ad5a012d593d6996f6c1b4025b
Darshan Ponikar <darshanp@geekyants.com>
2022-03-25 21:39:36 +0530

    Adding SEO stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { Projects } from "../src/components/Projects";
 import { Talks } from "../src/components/Talks";
 import { BlogRes, CaseStudyType, ProjectRes } from "../@types/res";
 import { getRequest } from "../src/utils/api";
+import Head from "next/head";
 // import { CaseStudy } from "../src/components/CaseStudy/CaseStudy";
 // import { getCaseStudyData } from "../src/data/casestudy";
 
@@ -17,18 +18,65 @@ interface HomeProps {
   casestudies: CaseStudyType[];
 }
 
-const Home: NextPage<HomeProps> = ({ projects, blogs, casestudies }) => {
+const Home: NextPage<HomeProps> = ({ projects, blogs }) => {
   return (
-    <div className="h-full">
-      <Header />
-      <Hero />
-      <Projects projects={projects} />
-      {/* <CaseStudy casestudies={casestudies} /> */}
-      <Blogs blogs={blogs} />
-      <Talks />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Darshan Ponikar | The Journey of Full Stack Developer. 👨🏻‍💻</title>
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content="Hi 👋. I am Darshan Ponikar. Working as Full Stack Developer at
+          Geekyants. Currently working with React + JavaScript/TypeScript. I am
+          Passionate about building Products. I explore new things on Weekends.
+          DM me for a quick chat."
+        />
+        <meta
+          name="keywords"
+          content="web dev, javascripts, css3, typescript, react, nextjs, frontend, backend, developer, learn"
+        />
+
+        <meta
+          property="og:title"
+          content=" Darshan Ponikar | The Journey of Full Stack Developer. 👨🏻‍💻"
+        />
+        <meta property="og:url" content="http://bonfireapp.in" />
+        <meta
+          property="og:image"
+          content="https://www.itsponikar.tech/assets/imgs/cover_image.png"
+        />
+        <meta property="og:site_name" content="Darshan Ponikar" />
+
+        <meta
+          name="twitter:title"
+          content="Darshan Ponikar | The Journey of Full Stack Developer. 👨🏻‍💻"
+        />
+        <meta
+          name="twitter:description"
+          content="Hi 👋. I am Darshan Ponikar. Working as Full Stack Developer at
+          Geekyants. Currently working with React + JavaScript/TypeScript. I am
+          Passionate about building Products. I explore new things on Weekends.
+          DM me for a quick chat."
+        />
+
+        <meta
+          name="twitter:image"
+          content="https://www.itsponikar.tech/assets/imgs/cover_image.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image:alt" content="Darshan's Portfolio Preview" />
+      </Head>
+      <div className="h-full">
+        <Header />
+        <Hero />
+        <Projects projects={projects} />
+        {/* <CaseStudy casestudies={casestudies} /> */}
+        <Blogs blogs={blogs} />
+        <Talks />
+        <Contact />
+        <Footer />
+      </div>
+    </>
   );
 };
 
